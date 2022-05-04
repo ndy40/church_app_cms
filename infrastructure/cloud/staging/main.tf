67c4b6eb659d4e036774cb6df408dd0ff1c492a7
Ndifreke <ndy40.ekott@gmail.com>
2022-05-04 22:38:45 +0100

    build(infrastructure): adding docker build step

--- infrastructure/cloud/staging/main.tf
+++ infrastructure/cloud/staging/main.tf
@@ -27,8 +27,8 @@ resource "digitalocean_droplet" "church-cms" {
       "export PATH=$PATH:/usr/bin",
       "sudo apt-get update",
       "sudo apt -y install software-properties-common",
-#      "sudo add-apt-repository --yes --update ppa:ansible/ansible",
-      "sudo apt -y install  ansible"
+      "sudo add-apt-repository --yes --update ppa:ansible/ansible",
+      "sudo apt -y install ansible"
     ]
   }
 
