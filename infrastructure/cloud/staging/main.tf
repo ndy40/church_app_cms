f22f5ea3eee0c670798c5888bdca41ae5c993b19
Ndifreke <ndy40.ekott@gmail.com>
2022-05-06 22:47:32 +0100

    build(infrastructure): adding postgres database

--- infrastructure/cloud/staging/main.tf
+++ infrastructure/cloud/staging/main.tf
@@ -32,7 +32,9 @@ resource "digitalocean_droplet" "church-cms" {
     inline = [
       "export PATH=$PATH:/usr/bin",
       "sudo apt update",
-      "sudo apt -y install software-properties-common python3-dev python3-pip",
+      "sudo add-apt-repository ppa:deadsnakes/ppa",
+      "sudo apt -y install software-properties-common python3.10 python3.10-dev python3-pip",
+      "sudp update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10",
       "python3 -m pip install -q --no-input ansible"
     ]
   }
