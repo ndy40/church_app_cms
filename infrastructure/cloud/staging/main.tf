e73debe52f479139cc34608242723d9e226e4a21
Ndifreke <ndy40.ekott@gmail.com>
2022-05-07 21:34:59 +0100

    build(infrastructure): adding postgres database

--- infrastructure/cloud/staging/main.tf
+++ infrastructure/cloud/staging/main.tf
@@ -1,10 +1,3 @@
-
-resource "random_string" "db_password" {
-  length = 16
-  min_upper = 3
-  override_special = "#%$"
-}
-
 resource "digitalocean_droplet" "church-cms" {
   image  = "ubuntu-20-04-x64"
   name   = "church-cms-staging"
@@ -34,25 +27,6 @@ resource "digitalocean_droplet" "church-cms" {
       "sudo apt update",
       "sudo add-apt-repository -y ppa:ansible/ansible",
       "sudo apt -y install software-properties-common ansible",
-    ]
-  }
-
-  provisioner "file" {
-    source = "../ansible-playbook"
-    destination = "/opt"
-  }
-
-  provisioner "remote-exec" {
-    connection {
-      host = self.ipv4_address
-      user = "root"
-      type = "ssh"
-      private_key = var.pvt_key
-      timeout = "2m"
-    }
-
-    inline = [
-      "ansible-playbook /opt/ansible-playbook/playbook.yml -e \"db_password='${random_string.db_password.result}'\""
     ]
   }
 }
--- infrastructure/cloud/staging/output.tf
+++ infrastructure/cloud/staging/output.tf
@@ -1,7 +1,3 @@
 output "server_id" {
   value = digitalocean_droplet.church-cms.ipv4_address
 }
-
-output "db_password" {
-  value = random_string.db_password.result
-}
