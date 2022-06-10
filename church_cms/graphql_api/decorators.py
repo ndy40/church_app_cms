4e2e529b21bc50ca2b1c306eaeb5c201a54fa812
Ndifreke <ndy40.ekott@gmail.com>
2022-06-11 00:26:22 +0100

    feat(device): adding me context

--- church_cms/graphql_api/decorators.py
+++ church_cms/graphql_api/decorators.py
@@ -9,7 +9,6 @@ from .constants import DEVICE_HEADER_KEY, ERROR_DEVICE_NOTFOUND
 def device_header_required(fn):
     @wraps(fn)
     def wrapper(cls, root, info, **kwargs):
-
         if DEVICE_HEADER_KEY in info.context.META:
             if Device.objects.filter(token=info.context.META.get(DEVICE_HEADER_KEY)).exists():
                 return fn(cls, root, info, **kwargs)
--- church_cms/graphql_api/query.py
+++ church_cms/graphql_api/query.py
@@ -1,7 +1,7 @@
 import graphene
 
-from .accounts.query import DeviceQuery
+from .accounts.query import MeContextType
 
 
-class Query(graphene.ObjectType, DeviceQuery):
-    hello = graphene.String(default_value='hi')
+class Query(MeContextType, graphene.ObjectType):
+    pass
